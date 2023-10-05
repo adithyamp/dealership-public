a3763b665ad170092f716d8fed5e8105c21a7f88
Adithya MP <adithyamp41@gmail.com>
2023-10-05 15:57:56 +0530

    code changes

--- Dealership/settings.py
+++ Dealership/settings.py
@@ -91,13 +91,20 @@ WSGI_APPLICATION = 'Dealership.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Dealership',
+#         'USER': 'postgres',
+#         'PASSWORD': '8747065541$$',
+#         'HOST': 'localhost',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Dealership',
-        'USER': 'postgres',
-        'PASSWORD': '8747065541$$',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
